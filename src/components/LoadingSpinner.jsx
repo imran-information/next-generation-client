4dfdeb35d3d0fa950edfb2af412bd68070252e74
imran <imranhossen0586@gmail.com>
2025-04-24 12:50:45 +0600

    Done

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
     return (
-        <div className='flex items-center justify-center w-full min-h-[calc(100vh-305px)] dark:bg-neutral-900 bg-bg' >
+        <div className='min-h-screen flex items-center justify-center w-full  dark:bg-neutral-900 bg-bg' >
             <div
                 aria-label='Loading...'
                 role='status'
